616decc1d4c04cc98220b39e85d8a05f6cec7e14
Richard Garrison <anm8tr@users.noreply.github.com>
2022-06-05 10:52:44 -0700

    Initial Commit

---
+++ Socialacademy/SocialacademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialacademyApp.swift
+//  Socialacademy
+//
+//  Created by Richard Garrison on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialacademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
